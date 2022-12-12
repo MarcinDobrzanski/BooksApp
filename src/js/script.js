d42c12d7807b0d74b26db63af8d0843a30a76d24
Marcin <marcin@dobrzanski.me>
2022-12-12 21:37:11 +0100

    Finish generated HTML with books

--- src/js/script.js
+++ src/js/script.js
@@ -16,31 +16,29 @@
 
   const templates = {
     booksList: Handlebars.compile(document.querySelector(select.templateOf.booksList).innerHTML),
-    // CODE ADDED START
 
-    // CODE ADDED END
   };
 
   function render() {
     
-    const booksHTML = document.querySelectorAll(select.containerOf.booksList);
+    const booksHTML = document.querySelector(select.containerOf.booksList);
 
     console.log('booksHTML', booksHTML);
 
-    for (let book in dataSource.books) {
+    for (let book of dataSource.books) {
 
       const generatedHTML = templates.booksList(book);
       console.log('generatedHTML', generatedHTML);
-      // create element using untils.createElementFromHTML
+
       const generatedDOM = utils.createDOMFromHTML(generatedHTML);
 
-      // find menu cointainer
-      const booksContainer = document.querySelector(select.containerOf.menu);
 
-      // add element to menu
-      booksHTML.appendChild(generatedDOM);
+      const booksContainer = document.querySelector(select.containerOf.booksList);
+
+
+      booksContainer.appendChild(generatedDOM);
     }
-    // generate HTML Based on template
+
   }
   render();
 
